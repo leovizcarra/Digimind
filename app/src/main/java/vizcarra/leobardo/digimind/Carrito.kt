0299bc08c6726db43da796c5554fd206a5066742
Leobardo Vizcarra <vizcarraleobardo@gmail.com>
2022-03-13 20:07:31 -0700

    Primer commit

---
+++ app/src/main/java/vizcarra/leobardo/digimind/Carrito.kt
@@ -0,0 +1,11 @@
+package vizcarra.leobardo.digimind
+
+import java.io.Serializable
+
+class Carrito:Serializable {
+    var recordatorios = ArrayList<Recordatorio>()
+
+    fun agregar(p:Recordatorio):Boolean{
+        return recordatorios.add(p)
+    }
+}
---
+++ app/src/main/java/vizcarra/leobardo/digimind/Recordatorio.kt
@@ -0,0 +1,7 @@
+package vizcarra.leobardo.digimind
+
+import java.io.Serializable
+
+data class Recordatorio (var dias:String,
+                         var tiempo:String,
+                         var nombre:String):Serializable
